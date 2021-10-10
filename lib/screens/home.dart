5b76a573a2c46599b09cf39e57e57574a6f2cb28
Jilal99 <82775029+Jilal99@users.noreply.github.com>
2021-10-09 22:45:57 -0400

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,162 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:midterm/provider/auth_provider.dart';
+import 'package:midterm/screens/login.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final double coverHeight = 280;
+  final double profileHeight = 150;
+  String? user = FirebaseAuth.instance.currentUser!.email ??
+      FirebaseAuth.instance.currentUser!.phoneNumber;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://i.ytimg.com/vi/pE3QWhb5nPU/maxresdefault.jpg"),
+                    fit: BoxFit.cover)),
+            child: Container(
+              width: double.infinity,
+              height: 200,
+              child: Container(
+                alignment: Alignment(0.0, 2.5),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg"),
+                  radius: 60.0,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 60,
+          ),
+          Text(
+            "Elon Musk",
+            style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.blueGrey,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w400),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Pretoria, South Africa",
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Technoking of Tesla || Founder of SpaceX",
+            style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Card(
+              margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              elevation: 2.0,
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                  child: Text(
+                    "Bio: I would like to die on Mars. Just not on impact.",
+                    style: TextStyle(
+                        letterSpacing: 2.0, fontWeight: FontWeight.w300),
+                  ))),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            "Joined on 08/23/1476",
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Friends",
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "33",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w300),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Followers",
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "10,000,000,000",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w300),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
